daa59f23365772832ba604b3beea7fa4099c9c5e
Allison Hamburger <aghamburger@gmail.com>
2015-04-24 10:51:08 -0400

    Working on finding center points of faces and placing gems there

--- bedazzle.py
+++ bedazzle.py
@@ -5,7 +5,7 @@ import math
 def run():
 	base = pickBaseObject()
 	g = makeGem()
-	placeGem(base, g)
+	findSpots(base, g)
 
 #return first object in the scene (for now) except for gemstones
 def pickBaseObject():
@@ -19,119 +19,94 @@ def pickBaseObject():
 
 #make gem geometry (one for now)
 def makeGem():
-	cmds.polyCube(name="gem")
+	cmds.polyCube(name="gem", w=.25, d=.25, h=.1)
 	cmds.polyMoveEdge( 'gem.e[1:2]', s=(.75, .75, .75) )
-	cmds.polyMoveEdge( 'gem.e[6:7]', s=(.75, .75, .75) )		#not sure this line is necessary
-	cmds.polyMoveEdge( 'gem.e[1:2]', t=(0, -.5, 0) )
+	# cmds.polyMoveEdge( 'gem.e[6:7]', s=(.75, .75, .75) )		#not sure this line is necessary
+	# cmds.polyMoveEdge( 'gem.e[1:2]', t=(0, -.5, 0) )
+
 
 #place gem on a 
-def placeGem(baseObj,gem):
+def findSpots(baseObj,gem):
 	
 	cmds.select('pCube1')
-	gem_dim = .05  #say gems are .05*.05 in uv space
+	centers = []
+	for face_i in range(cmds.polyEvaluate('pCube1', f=True)):
+		face = cmds.select('pCube1.f['+ str(face_i)+']')
 
-	# get UV bounding box
-	bounds = cmds.polyEvaluate(b2 = True)
+		normal = cmds.polyInfo(fn=True)
+		verts = cmds.polyListComponentConversion(tv = True)
 
-	gem_rows = int(100*(bounds[1][1]-bounds[1][0])/gem_dim)/100		#not sure if this actually helps precision...	
-	gem_cols = int(100*(bounds[0][1]-bounds[0][0])/gem_dim)/100
+		positions = []
 
-	to_place = getGemsToPlace(gem_rows, gem_cols, gem_dim, bounds)
+		for vert_i in range(cmds.polyEvaluate(verts, v=True)):
+			cmds.select('pCube1.vtx['+ str(vert_i)+']')
+			positions.append(cmds.xform(query=True, translation=True, worldSpace=True))
+		
+		avg=[0,0,0]
+		for p in positions:
+			avg[0] += p[0]/4
+			avg[1] += p[1]/4
+			avg[2] += p[2]/4
+		centers.append(avg)
 
-	#for uv_coord in to_place:
-		#convert to world coord
-		#get normal
-		#make or duplicate gem, translated to that spot, rotated to normal
+		for c in centers:		
+			placeGem(c,normal,gem)
 
-
-def getGemsToPlace(rows, cols, dim, bounds):
-	#get UV points
-	cmds.select(cmds.polyListComponentConversion('pCube1.vtx[*]',tuv = True))
-	uvs = cmds.polyEditUV( query=True )
-
-	#need to run mel script that finds borders of uvs /convert that to python
-	uvs_and_angles = makeConvexIshHull(uvs,bounds)
-	gem_uv_coords = []
-	anchor = (uvs[uvs_and_angles[0][0]], uvs[uvs_and_angles[0][0]+1])
-
-	for r in range(rows):
-		for c in range(cols):
-			center = ((r+.5)*dim, (c+.5)*dim);	#center point of gem
-			if (wantToKeep(center, uvs_and_angles, anchor, uvs)):
-				gem_uv_coords.append(center)
-
-	print len(gem_uv_coords)
+def placeGem(pt, norm, gem):
+	cmds.select('gem')
+	cmds.duplicate()
+	# cmds.rotate(norm)
+	cmds.move(pt[0], pt[1],pt[2])
 
 
-def makeConvexIshHull(uvs,bounds):
-	angles = []
-	anchor = (0,0)
-	#select anchor point (max y value). should add a check here that uvs are loaded properly
-	for u in range(1, len(uvs), 2):
-		if uvs[u] == bounds[1][1]:
-			anchor = (uvs[u-1], uvs[u])
-			angles.append((u-1, 0))
-			break
+#not using this probably:
+# script that returns the center point of selected polygon faces. 
+# owen burgess 2009
+# http://forums.cgsociety.org/archive/index.php/t-1034715.html
 
-	#build list of tuples where x[0] = index in uv list, x[1] = angle with anchor
-	for i in range(1, len(uvs)-1, 2):
-		if i == u-1:	#anchor index
-			continue
-		pt = (uvs[i], uvs[i+1])
-		angle_with_anchor = getAngle(anchor, pt)
-		angles.append((i, angle_with_anchor))
+def faceCenter():
 
-	#sort by angle 
-	sorted_angles = sorted(angles, key = lambda x: x[1])
-	return sorted_angles
+	faceCenter = []
+	selection = OM.MSelectionList()
+	OM.MGlobal.getActiveSelectionList(selection)
+	iter = OM.MItSelectionList (selection, OM.MFn.kMeshPolygonComponent)
 
+	while not iter.isDone():
+		status = OM.MStatus
+		dagPath = OM.MDagPath()
+		component = OM.MObject()
 
-#get angle between anchor and pt
-def getAngle(anchor, pt):
-	vecX = (pt[0]-anchor[0])/ math.sqrt( (anchor[0] - pt[0])**2 + (anchor[1] - pt[1])**2 )
-	#take dot prod with (1,0), which is just the x component.
-	return math.acos(vecX)
+		iter.getDagPath(dagPath, component)
 
-def wantToKeep(pt, hull, anchor, uvs):
-	angle = getAngle(anchor,pt)
+		polyIter = OM.MItMeshPolygon(dagPath, component)
 
-	#consider changing this to use a BST? technically this adds O(size of hull) except gems are evenly distributed so will be less
-	for h in range(len(hull)):
-		if angle < hull[h][1]:
-			hullpt_1 = (uvs[hull[h-1][0]],uvs[hull[h-1][0]+1])		# h should never be 0 because angle between p and anchor >= 0
-			hullpt_2 = (uvs[hull[h][0]],uvs[hull[h][0]+1])
-			if isConvex(pt, hullpt_1, hullpt_2):
-				return False	#outside of the points, don't want a gem
-			return True;
+		while not polyIter.isDone():
 
-def isConvex(pt, h1, h2):
-	return (pt[0] - h2[0])*(h2[1]-h1[1])-(pt[1]-h1[1])*(h2[0]-h1[0]) > 0
+			i = 0
+			i = polyIter.index()
+			faceInfo = [0]
+			faceInfo[0] = ("The center point of face %s is:" %i)
+
+			center = OM.MPoint
+			center = polyIter.center(OM.MSpace.kWorld)
+			point = [center.x,center.y,center.z]
+
+			faceCenter.append(point)
+
+			polyIter.next()
+
+		iter.next()
+
+	return faceCenter
 
 #made this a function ~*~very temporarily~*~ so I can collapse notes in sublime
 def notes():
 	"""
-	GAME PLAN:
-	Get base object vertices
-	Convert to UV coordinates
-
-	(assume UVs are continuously connected, for now)
-	Find width and height of bounding box of UVs (min/max x/y values)
-	Get number of gems needed (ie how many rows and columns) by taking bounding box width / gem width and same for heights
-	Figure out upper left corner (or center?) of all these gems in terms of UV coordinates
-
-	Convex hull:
-	sort UV coordinates in order based on angle with an anchor point
-	Iterate over gem rows
-		If center point(could also maybe check all 4 corners?) makes convex angle w closest 2 vertices (ie outside of them) 
-			remove it from list
-
-	Convert UVs of gems to world space
-	Get normals at those world space points
-	Translate and rotate gems so they are at the world space point, facing up
-
-
-	Still need to address:
-	UVs being disconnected
-	How to select base object
+	THE NEW PLAN:
+	Iterate over faces
+	Find midpoint of each faces
+		Place gem
+	Spiral around that midpoint, placing gems using a vector that lies in the plane (perpendicular to an edge? random?)
+	fin.
 
 	"""
