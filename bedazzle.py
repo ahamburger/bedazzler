c436ad168a0bb558dde42ed7257dc4c5a7cf429e
Allison Hamburger <aghamburger@gmail.com>
2015-04-23 22:28:55 -0400

    Gets maybe correct points but can't go from UV -> world so about to try different approach

--- bedazzle.py
+++ bedazzle.py
@@ -17,7 +17,6 @@ def pickBaseObject():
 		if "gem" not in o:
 			return o
 
-
 #make gem geometry (one for now)
 def makeGem():
 	cmds.polyCube(name="gem")
@@ -29,23 +28,42 @@ def makeGem():
 def placeGem(baseObj,gem):
 	
 	cmds.select('pCube1')
+	gem_dim = .05  #say gems are .05*.05 in uv space
 
 	# get UV bounding box
 	bounds = cmds.polyEvaluate(b2 = True)
-	print bounds
-	gem_rows = [x for x in range(int(100*(bounds[1][1]-bounds[1][0])/.05)/100)]	#say gems are .05*.05 in uv space
-	gem_cols = [x for x in range(int(100*(bounds[0][1]-bounds[0][0])/.05)/100)]
 
-	to_place = getGemsToPlace(gem_rows, gem_cols)
+	gem_rows = int(100*(bounds[1][1]-bounds[1][0])/gem_dim)/100		#not sure if this actually helps precision...	
+	gem_cols = int(100*(bounds[0][1]-bounds[0][0])/gem_dim)/100
 
-def getGemsToPlace(rows, cols):
+	to_place = getGemsToPlace(gem_rows, gem_cols, gem_dim, bounds)
+
+	#for uv_coord in to_place:
+		#convert to world coord
+		#get normal
+		#make or duplicate gem, translated to that spot, rotated to normal
+
+
+def getGemsToPlace(rows, cols, dim, bounds):
 	#get UV points
 	cmds.select(cmds.polyListComponentConversion('pCube1.vtx[*]',tuv = True))
 	uvs = cmds.polyEditUV( query=True )
 
-	uvs_to_check = makeConvexIshHull(uvs)
+	#need to run mel script that finds borders of uvs /convert that to python
+	uvs_and_angles = makeConvexIshHull(uvs,bounds)
+	gem_uv_coords = []
+	anchor = (uvs[uvs_and_angles[0][0]], uvs[uvs_and_angles[0][0]+1])
 
-def makeConvexIshHull(uvs)
+	for r in range(rows):
+		for c in range(cols):
+			center = ((r+.5)*dim, (c+.5)*dim);	#center point of gem
+			if (wantToKeep(center, uvs_and_angles, anchor, uvs)):
+				gem_uv_coords.append(center)
+
+	print len(gem_uv_coords)
+
+
+def makeConvexIshHull(uvs,bounds):
 	angles = []
 	anchor = (0,0)
 	#select anchor point (max y value). should add a check here that uvs are loaded properly
@@ -54,8 +72,6 @@ def makeConvexIshHull(uvs)
 			anchor = (uvs[u-1], uvs[u])
 			angles.append((u-1, 0))
 			break
-
-	print anchor
 
 	#build list of tuples where x[0] = index in uv list, x[1] = angle with anchor
 	for i in range(1, len(uvs)-1, 2):
@@ -67,39 +83,55 @@ def makeConvexIshHull(uvs)
 
 	#sort by angle 
 	sorted_angles = sorted(angles, key = lambda x: x[1])
-	print sorted_angles
+	return sorted_angles
 
 
-
+#get angle between anchor and pt
 def getAngle(anchor, pt):
 	vecX = (pt[0]-anchor[0])/ math.sqrt( (anchor[0] - pt[0])**2 + (anchor[1] - pt[1])**2 )
 	#take dot prod with (1,0), which is just the x component.
 	return math.acos(vecX)
 
+def wantToKeep(pt, hull, anchor, uvs):
+	angle = getAngle(anchor,pt)
 
-"""
-GAME PLAN:
-Get base object vertices
-Convert to UV coordinates
+	#consider changing this to use a BST? technically this adds O(size of hull) except gems are evenly distributed so will be less
+	for h in range(len(hull)):
+		if angle < hull[h][1]:
+			hullpt_1 = (uvs[hull[h-1][0]],uvs[hull[h-1][0]+1])		# h should never be 0 because angle between p and anchor >= 0
+			hullpt_2 = (uvs[hull[h][0]],uvs[hull[h][0]+1])
+			if isConvex(pt, hullpt_1, hullpt_2):
+				return False	#outside of the points, don't want a gem
+			return True;
 
-(assume UVs are continuously connected, for now)
-Find width and height of bounding box of UVs (min/max x/y values)
-Get number of gems needed (ie how many rows and columns) by taking bounding box width / gem width and same for heights
-Figure out upper left corner (or center?) of all these gems in terms of UV coordinates
+def isConvex(pt, h1, h2):
+	return (pt[0] - h2[0])*(h2[1]-h1[1])-(pt[1]-h1[1])*(h2[0]-h1[0]) > 0
 
-Convex hull:
-sort UV coordinates in order based on angle with an anchor point
-Iterate over gem rows
-	If center point(could also maybe check all 4 corners?) makes convex angle w closest 2 vertices (ie outside of them) 
-		remove it from list
+#made this a function ~*~very temporarily~*~ so I can collapse notes in sublime
+def notes():
+	"""
+	GAME PLAN:
+	Get base object vertices
+	Convert to UV coordinates
 
-Convert UVs of gems to world space
-Get normals at those world space points
-Translate and rotate gems so they are at the world space point, facing up
+	(assume UVs are continuously connected, for now)
+	Find width and height of bounding box of UVs (min/max x/y values)
+	Get number of gems needed (ie how many rows and columns) by taking bounding box width / gem width and same for heights
+	Figure out upper left corner (or center?) of all these gems in terms of UV coordinates
+
+	Convex hull:
+	sort UV coordinates in order based on angle with an anchor point
+	Iterate over gem rows
+		If center point(could also maybe check all 4 corners?) makes convex angle w closest 2 vertices (ie outside of them) 
+			remove it from list
+
+	Convert UVs of gems to world space
+	Get normals at those world space points
+	Translate and rotate gems so they are at the world space point, facing up
 
 
-Still need to address:
-UVs being disconnected
-How to select base object
+	Still need to address:
+	UVs being disconnected
+	How to select base object
 
-"""
+	"""
